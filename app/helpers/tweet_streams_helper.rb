06655ecf1c9d705d68d329e6fcb8aa58626087d7
Will Lee-Wagner <wleewagner@assetavenue.com>
2016-12-19 11:52:34 -0800

    add @ mentions

--- app/helpers/tweet_streams_helper.rb
+++ app/helpers/tweet_streams_helper.rb
@@ -1,2 +1,13 @@
 module TweetStreamsHelper
+  def render_tweet_text(text)
+    words = text.split.map do |word|
+      if word[0] == '@'
+        link_to(word, tweet_stream_path(id: word[1..-1]))
+      else
+        word
+      end
+    end
+
+    words.join(' ').html_safe
+  end
 end
